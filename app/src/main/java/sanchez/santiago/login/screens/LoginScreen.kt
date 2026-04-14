aec737e1df03121bece11ba960ee39f4454a5345
a <>
2026-04-14 15:38:26 -0600

    av

---
+++ app/src/main/java/sanchez/santiago/login/screens/LoginScreen.kt
@@ -0,0 +1,51 @@
+package sanchez.santiago.login.screens
+
+import android.R
+import android.graphics.Outline
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoginScreen(){
+
+    var user by remember { mutableStateOf("") }
+    var pass by remember { mutableStateOf("") }
+
+    Column() {
+        Text("Inicio de sesión", style = MaterialTheme.typography.displayLarge)
+        Spacer(modifier= Modifier.height(16.dp))
+        OutlinedTextField(value =user, onValueChange = {user = it},
+            label = {Text("Nombre de usuario")},
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(value = pass, onValueChange = {pass = it},
+            label = {Text("Contrasena")},
+            modifier = Modifier.fillMaxWidth())
+
+        Spacer(Modifier.height(30.dp))
+        Button(onClick={}, modifier = Modifier.fillMaxWidth()){
+        Text("Ingresar")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPreview(){
+    LoginScreen()
+}
